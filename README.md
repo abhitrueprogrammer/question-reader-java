21e0373592eed8d4f956639f7afa12a9170e0e7c
abhitrueprogrammer <abhiaruvs3@gmail.com>
2024-11-21 15:54:47 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Everything I learnt in java
 
 This project is a showcase of everything I learnt in java as of Nov 21 2024.
+
+Takes in questions from a file named 'f.txt', asks users the answers and stores them on a file 'f.ser' with answers.
--- file.java
+++ file.java
@@ -3,23 +3,25 @@
 import java.io.*;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.Scanner;
 
 public class file {
+    static Scanner scan = new Scanner(System.in);
     @SuppressWarnings("unchecked")
-    public static void main(String args[]) {
-
+    public static void main(String args[]) throws EmptyFile {
         try {
             // BufferedWriter f = new BufferedWriter(new FileWriter("f.txt"));
             BufferedReader file = new BufferedReader(new FileReader("f.txt"));
             String line;
             ArrayList<String> lines = new ArrayList<>();
+            boolean empty = true;
             while ((line = file.readLine()) != null) {
+                empty = false;
                 String lineArr[] = line.split("\\?");
                 for(int i =0; i< lineArr.length; i++)
                 {
                     if(lineArr[i].length() < 2)
                     {
-
                         continue;
                     }
                     String lower = lineArr[i].toLowerCase();
@@ -35,11 +37,16 @@ public class file {
                         }
                     }
                     if(isDup) continue;
-
+                    
                     lines.add(lower);
-
+                    
                 }
             
+            }
+            if(empty)
+            {
+                file.close();
+                throw new EmptyFile("FileEmpty");
             }
             LinkedHashMap<Integer, String> lm = new LinkedHashMap<>();
             for(int i = 0; i < lines.size(); i++)
@@ -49,21 +56,26 @@ public class file {
             ObjectOutputStream f = new ObjectOutputStream(new FileOutputStream("f.ser"));
             for(Integer key: lm.keySet())
             {
-                QuestionsWtime<Integer> q= new QuestionsWtime<>(lm.get(key), 0, 10);
+                System.out.println("Answer for: " + lm.get(key));
+                String answer =  scan.nextLine();
+                QuestionsWanswers<String> q= new QuestionsWanswers<>(lm.get(key), 0, answer);
                 f.writeObject(q);
             }
             ObjectInputStream fr = new ObjectInputStream(new FileInputStream("f.ser"));
             Object obj;
+            System.out.println("Question answers stored are: ");
             try
             {
             
                 while(true)
                 {
                     obj = fr.readObject();
-                    if(obj instanceof QuestionsWtime)
+                    if(obj instanceof QuestionsWanswers)
                     {
-                        QuestionsWtime<Integer> q = (QuestionsWtime<Integer>) obj;
-                        System.out.println(q.question );
+                        QuestionsWanswers<String> q = (QuestionsWanswers<String>) obj;
+                        System.out.println("Q: "+ q.question );
+                        System.out.println("A: "+q.answer);
+                        System.out.println();
                     } 
                 }
             }
@@ -75,20 +87,34 @@ public class file {
             {
 
             }
-            f.close();
-            fr.close();
-            file.close();
+            finally
+            {
+                f.close();
+                fr.close();
+                file.close();   
+            }
+            
 
         } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         } 
+        finally
+        {
+          
+        }
 
         // BufferedReader f = new BufferedReader(new FileReader("f.txt"));
     }
 
 }
-
+class EmptyFile extends Exception
+{
+    EmptyFile(String msg)
+    {
+        super(msg);
+    }
+}
 class Questions implements Serializable {
     String question = "";
     int marks = 0;
@@ -99,12 +125,12 @@ class Questions implements Serializable {
     }
 }
 
-class QuestionsWtime<timeType> extends Questions {
-    timeType time;
+class QuestionsWanswers<answerType> extends Questions {
+    answerType answer;
 
-    QuestionsWtime(String question, int marks, timeType time) {
+    QuestionsWanswers(String question, int marks, answerType answer) {
         super(question, marks);
-        this.time = time;
+        this.answer = answer;
     }
 
 }
